25355256004ddf48e93d2a3cb7b161b04cb17617
Andy Tran <aandy.tran@mail.utoronto.ca>
2024-08-23 12:22:13 -0700

    Fixed several bugs in the HuffmanTree class as well as finishing up the HuffmanTree creation in the Compressor class.

--- Compressor.java
+++ Compressor.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 
 public class Compressor {
 
-    // Function to build the frequency dictionary
-    public static HashMap<Integer, Integer> buildFrequencyDict(byte[] text) {
+    // Build the frequency map
+    public static HashMap<Integer, Integer> buildFrequencyMap(byte[] text) {
         HashMap<Integer, Integer> freqMap = new HashMap<>();
         for (byte b : text) {
             int key = Byte.toUnsignedInt(b);  // Convert byte to unsigned int
@@ -17,44 +17,49 @@ public class Compressor {
         return freqMap;
     }
 
-    public static HuffmanTree buildHuffmanTree(HashMap<Integer, Integer> freqDict) {
-        if (freqDict.size() == 1) {
-            Integer symbol = freqDict.keySet().iterator().next();
-            return new HuffmanTree(symbol, new HuffmanTree(symbol), null);
-        }
+    // Create a HuffmanTree for each symbol in the frequency map
+    public static ArrayList<HuffmanTree> buildHuffmanTreeList(HashMap<Integer, Integer> freqDict) {
 
-        if (freqDict.isEmpty()) {
-            return new HuffmanTree(null);
-        }
-
-        List<HuffmanTree> store = new ArrayList<>();
-
+        ArrayList<HuffmanTree> huffmanTreeList = new ArrayList<>();
+        
         for (Map.Entry<Integer, Integer> entry : freqDict.entrySet()) {
-            HuffmanTree tree = new HuffmanTree(entry.getKey());
-            tree.number = entry.getValue();
-            store.add(tree);
+            Integer symbol = entry.getKey();
+            Integer freq = entry.getValue();
+            HuffmanTree treeItem = new HuffmanTree(symbol, freq);
+            huffmanTreeList.add(treeItem);
         }
 
-        while (store.size() > 1) {
-            store.sort(Comparator.comparingInt(t -> t.number));
-            HuffmanTree lowest = store.remove(0);
-            HuffmanTree secondLowest = store.remove(0);
+        return huffmanTreeList;
 
-            HuffmanTree tree = new HuffmanTree(null, lowest, secondLowest);
-            tree.number = lowest.number + secondLowest.number;
-            store.add(tree);
-        }
-
-        HuffmanTree resultTree = store.get(0);
-        List<HuffmanTree> cleanser = treeToList(resultTree);
-
-        for (HuffmanTree tree : cleanser) {
-            tree.number = 0;
-        }
-
-        return resultTree;
     }
 
+    // Merge the Huffman trees into a final form
+    public static HuffmanTree HuffmanTreeMerger(ArrayList<HuffmanTree> treeList) {
+
+        while (treeList.size() != 1) {
+            HuffmanTree smallest = null;
+            HuffmanTree secondSmallest = null;
+
+            for (HuffmanTree tree : treeList) {
+                if (smallest == null || tree.size < smallest.size) {
+                    secondSmallest = smallest;
+                    smallest = tree;
+                } 
+
+                else if (secondSmallest == null || tree.size < secondSmallest.size) {
+                    secondSmallest = tree;
+                }
+            }
+
+            treeList.remove(smallest);
+            treeList.remove(secondSmallest);
+
+            HuffmanTree mergedTree = new HuffmanTree(null, smallest, secondSmallest, smallest.size + secondSmallest.size);
+            treeList.add(mergedTree);
+        }
+
+        return treeList.get(0);
+    }
 
 
 
@@ -101,10 +106,13 @@ public class Compressor {
            return;
        } 
        
-        HashMap<Integer, Integer> b = buildFrequencyDict(fileInByte);
+        HashMap<Integer, Integer> b = buildFrequencyMap(fileInByte);
         System.out.println(b);
-        HuffmanTree testTree = buildHuffmanTree(b);
-        System.out.println(testTree);
+        ArrayList<HuffmanTree> testTreeList = buildHuffmanTreeList(b);
+        System.out.println(testTreeList);
+
+        HuffmanTree finalTree = HuffmanTreeMerger(testTreeList);
+        System.out.println(finalTree);
        
     }
 }
--- HuffmanTree.java
+++ HuffmanTree.java
@@ -1,54 +1,79 @@
-import java.util.Optional;
 
 public class HuffmanTree {
     /**
      * Public Attributes:
      * ===========
      * symbol: symbol stored in this Huffman tree node, if any
-     * number: the number of this Huffman tree node
+     * size: the size of this Huffman tree node
      * left: the left subtree of this Huffman tree node
      * right: the right subtree of this Huffman tree node
      */
     public Integer symbol;
-    public Integer number;
+    public Integer size;
     public HuffmanTree left;
     public HuffmanTree right;
 
     // Constructor
-    public HuffmanTree(Integer symbol, HuffmanTree left, HuffmanTree right) {
+    public HuffmanTree(Integer symbol, HuffmanTree left, HuffmanTree right, Integer size) {
         this.symbol = symbol;
         this.left = left;
         this.right = right;
-        this.number = null;
+        this.size = size;
     }
 
     // Constructor for leaf
-    public HuffmanTree(Integer symbol) {
-        this(symbol, null, null); // Call the main constructor with null for left and right
+    public HuffmanTree(Integer symbol, Integer size) {
+        this(symbol, null, null, size); // Call the main constructor with null for left and right
     }
 
     // Check if this HuffmanTree is equivalent to another HuffmanTree
     @Override
     public boolean equals(Object otherObj) {
         if (this == otherObj) {
-            return true;
+            return true;  // Same object reference
         }
-
+    
         if (otherObj == null || getClass() != otherObj.getClass()) {
+            return false;  // Null or different class
+        }
+    
+        HuffmanTree otherTree = (HuffmanTree) otherObj;
+    
+        // Compare symbols considering possible nulls
+        if (this.symbol == null) {
+            if (otherTree.symbol != null) {
+                return false;
+            }
+        } else if (!this.symbol.equals(otherTree.symbol)) {
             return false;
         }
-
-        HuffmanTree otherTree = (HuffmanTree) otherObj;
-        return symbol.equals(otherTree.symbol) 
-                && ((left == null && otherTree.left == null) || (left != null && left.equals(otherTree.left))) 
-                && ((right == null && otherTree.right == null) || (right != null && right.equals(otherTree.right)));
-
+    
+        // Compare left children
+        if (this.left == null) {
+            if (otherTree.left != null) {
+                return false;
+            }
+        } else if (!this.left.equals(otherTree.left)) {
+            return false;
+        }
+    
+        // Compare right children
+        if (this.right == null) {
+            if (otherTree.right != null) {
+                return false;
+            }
+        } else if (!this.right.equals(otherTree.right)) {
+            return false;
+        }
+    
+        return true;  // All checks passed
     }
+    
 
     // Return a string representation of this HuffmanTree
     @Override
     public String toString() {
-        return "HuffmanTree(" + symbol + ", " + left + ", " + right + ")";
+        return "HuffmanTree(" + this.symbol + ", " + left + ", " + right + ")";
     }
 
     // Check if this HuffmanTree is a leaf
@@ -59,25 +84,25 @@ public class HuffmanTree {
     // Return the number of nodes required to represent this Huffman tree
     // Precondition: This Huffman tree is already numbered.
     public byte[] numNodesToBytes() {
-        return new byte[]{(byte) (number + 1)};
+        return new byte[]{(byte) (size + 1)};
     }
 
 
 
     // Test Space (REMOVE IN FINAL VERSION)
     public static void main(String[] args) {
-        HuffmanTree a = new HuffmanTree(4);
-        HuffmanTree b = new HuffmanTree(4);
+        HuffmanTree a = new HuffmanTree(4, 1);
+        HuffmanTree b = new HuffmanTree(4, 2);
 
         System.out.println(a.equals(b)); // Should print: true
 
-        b = new HuffmanTree(5);
+        b = new HuffmanTree(5, 2);
         System.out.println(a.equals(b)); // Should print: false
 
         System.out.println(a.isLeaf()); // Should print: true
         System.out.println(a.toString()); // Should print: HuffmanTree(4, null, null)
 
-        HuffmanTree c = new HuffmanTree(6, a, b);
+        HuffmanTree c = new HuffmanTree(6, a, b, 4);
         System.out.println(c.toString());// Should print: HuffmanTree(6, HuffmanTree(4, null, null), HuffmanTree(5, null, null))
     }
 }
