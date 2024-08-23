323a362e21924b62cd3ba9d345ace612a173d1ba
Andy Tran <aandy.tran@mail.utoronto.ca>
2024-08-23 13:47:21 -0700

    Finished encoding logic

--- Compressor.java
+++ Compressor.java
@@ -1,19 +1,22 @@
-import java.util.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.io.IOException;
+import java.util.*;
 
 public class Compressor {
 
     // Build the frequency map
     public static HashMap<Integer, Integer> buildFrequencyMap(byte[] text) {
+
         HashMap<Integer, Integer> freqMap = new HashMap<>();
+
         for (byte b : text) {
-            int key = Byte.toUnsignedInt(b);  // Convert byte to unsigned int
+            int key = Byte.toUnsignedInt(b); 
             // System.out.println(key);
             freqMap.put(key, freqMap.getOrDefault(key, 0) + 1);
         }
+
         return freqMap;
     }
 
@@ -61,8 +64,38 @@ public class Compressor {
         return treeList.get(0);
     }
 
+    // Encodes all symbols in the final Huffman tree into their respective codes
+    public static HashMap<Integer, String> HuffmanEncoder(HuffmanTree tree) {
+        HashMap<Integer, String> codeMap = new HashMap<>();
 
+        if (tree.left != null && !tree.left.isLeaf()) {
+            HashMap<Integer, String> leftCodes = HuffmanEncoder(tree.left);
 
+            for (HashMap.Entry<Integer, String> entry : leftCodes.entrySet()) {
+                codeMap.put(entry.getKey(), "0" + entry.getValue());
+            }
+
+        } 
+
+        else if (tree.left != null && tree.left.isLeaf()) {
+            codeMap.put(tree.left.symbol, "0");
+        }
+
+        if (tree.right != null && !tree.right.isLeaf()) {
+            HashMap<Integer, String> rightCodes = HuffmanEncoder(tree.right);
+
+            for (HashMap.Entry<Integer, String> entry : rightCodes.entrySet()) {
+                codeMap.put(entry.getKey(), "1" + entry.getValue());
+            }
+
+        } 
+        
+        else if (tree.right != null && tree.right.isLeaf()) {
+            codeMap.put(tree.right.symbol, "1");
+        }
+
+        return codeMap;
+    }
 
 
 
@@ -114,6 +147,9 @@ public class Compressor {
         HuffmanTree finalTree = HuffmanTreeMerger(testTreeList);
         System.out.println(finalTree);
        
+        HashMap<Integer, String> symbolCode = HuffmanEncoder(finalTree);
+        System.out.println(symbolCode);
+
     }
 }
 
