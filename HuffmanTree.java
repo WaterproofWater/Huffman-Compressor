c40fde8fac958af3e76bb4848620903a040c4d6c
Andy Tran <aandy.tran@mail.utoronto.ca>
2024-08-26 22:30:13 -0700

    Removed main function that was used for testings during development.

--- HuffmanTree.java
+++ HuffmanTree.java
@@ -21,7 +21,7 @@ public class HuffmanTree {
         this.size = size;
     }
 
-    // Constructor for leaf
+    // Constructor for leaf node
     public HuffmanTree(Integer symbol, Integer size) {
         this(symbol, null, null, size); // Call the main constructor with null for left and right
     }
@@ -38,38 +38,45 @@ public class HuffmanTree {
         }
     
         HuffmanTree otherTree = (HuffmanTree) otherObj;
-    
+
         // Compare symbols considering possible nulls
         if (this.symbol == null) {
             if (otherTree.symbol != null) {
                 return false;
             }
-        } else if (!this.symbol.equals(otherTree.symbol)) {
+        } 
+        
+        else if (!this.symbol.equals(otherTree.symbol)) {
             return false;
         }
     
+
         // Compare left children
         if (this.left == null) {
             if (otherTree.left != null) {
                 return false;
             }
-        } else if (!this.left.equals(otherTree.left)) {
+        } 
+        
+        else if (!this.left.equals(otherTree.left)) {
             return false;
         }
     
+
         // Compare right children
         if (this.right == null) {
             if (otherTree.right != null) {
                 return false;
             }
-        } else if (!this.right.equals(otherTree.right)) {
+        } 
+        
+        else if (!this.right.equals(otherTree.right)) {
             return false;
         }
     
-        return true;  // All checks passed
+        return true; 
     }
     
-
     // Return a string representation of this HuffmanTree
     @Override
     public String toString() {
@@ -85,24 +92,5 @@ public class HuffmanTree {
     // Precondition: This Huffman tree is already numbered.
     public byte[] numNodesToBytes() {
         return new byte[]{(byte) (size + 1)};
-    }
-
-
-
-    // Test Space (REMOVE IN FINAL VERSION)
-    public static void main(String[] args) {
-        HuffmanTree a = new HuffmanTree(4, 1);
-        HuffmanTree b = new HuffmanTree(4, 2);
-
-        System.out.println(a.equals(b)); // Should print: true
-
-        b = new HuffmanTree(5, 2);
-        System.out.println(a.equals(b)); // Should print: false
-
-        System.out.println(a.isLeaf()); // Should print: true
-        System.out.println(a.toString()); // Should print: HuffmanTree(4, null, null)
-
-        HuffmanTree c = new HuffmanTree(6, a, b, 4);
-        System.out.println(c.toString());// Should print: HuffmanTree(6, HuffmanTree(4, null, null), HuffmanTree(5, null, null))
     }
 }
